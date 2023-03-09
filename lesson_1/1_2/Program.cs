ea31222cd6c4024fb27c23eda2946ec66cf2c41f
Ivan <ubahkul@gmail.com>
2023-03-09 16:23:44 +0300

    1_2

--- lesson_1/1_2/Program.cs
+++ lesson_1/1_2/Program.cs
@@ -3,37 +3,37 @@
 
 Console.Write("Введите число от 1 до 7: ");
 int num = int.Parse(Console.ReadLine()!);
-if(num == 1);
+if(num == 1)
 {
     Console.Write("понедельник");
 }
 
-else if(num == 2);
+else if(num == 2)
 {
     Console.Write("вторник");
 }
 
-else if(num == 3);
+else if(num == 3)
 {
     Console.Write("среда");
 }
 
-else if(num == 4);
+else if(num == 4)
 {
     Console.Write("четверг");
 }
 
-else if(num == 5);
+else if(num == 5)
 {
     Console.Write("пятница");
 }
 
-else if(num == 6);
+else if(num == 6)
 {
     Console.Write("суббота");
 }
 
-else if(num == 7);
+else if(num == 7)
 {
     Console.Write("воскресенье");
 }
