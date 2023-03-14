4df9497e253f2dc493bd45963347b1c1c9e87fc2
Ivan <ubahkul@gmail.com>
2023-03-14 09:38:41 +0300

    HW_1

---
+++ lesson_1/HW/1_0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая на вход принимает два числа и 
+// выдает, какое число больше, а какое меньше.
+// a = 5; b = 7 -> max = 7
+// a =2; b = 10 -> max = 10
+// a = -9; b = -3 -> max = -3
+string num1 = Console.ReadLine()!;
+string num2 = Console.ReadLine()!;
+
+int n1 = int.Parse(num1);
+int n2 = int.Parse(num2);
+
+if (n1 > n2)
+{
+    Console.Write("num1 - большее num2 - меньшее");
+}
+else if (n1 == n2)
+{
+    Console.Write("num1 и num2 равные");
+}
+else
+{
+    Console.Write("num1 - меньшее num2 - большее");
+}
---
+++ lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая принимает на вход три числа
+// и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+int num3 = int.Parse(Console.ReadLine()!);
+
+if (num1 < num2)
+{
+    num1 = num2;
+}
+if (num1 < num3)
+{
+    num1 = num2;
+}
+Console.Write($"The number {num1} is the largest!");
