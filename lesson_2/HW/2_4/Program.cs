13b63ec422fe437122ae0c5fe37c0dd771056d98
Ivan <ubahkul@gmail.com>
2023-03-17 14:26:05 +0300

    HW_2

---
+++ lesson_2/HW/2_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая выводит
+// случайное трёхзначное число и удаляет вторую цифру
+// этого числа.
+
+string FirstThird(int num)
+{
+    if (num > 99 && num < 1000)
+        return $"{num} -> {num / 100 * 10 + num % 10}";
+    return "Two-digit number!";
+}
+
+int val = int.Parse(Console.ReadLine()!);
+string result = FirstThird(val);
+Console.WriteLine(result);
