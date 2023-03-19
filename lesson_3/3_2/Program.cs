f4fc7149de0d92f6db79b0dfda94d038f810acb6
Ivan <ubahkul@gmail.com>
2023-03-19 23:01:44 +0300

    HW_3

--- lesson_3/3_2/Program.cs
+++ lesson_3/3_2/Program.cs
@@ -15,7 +15,8 @@ int x2=int.Parse(Console.ReadLine()!);
 Console.Write("y2=");
 int y2=int.Parse(Console.ReadLine()!);
 
-double Length (int x1, int y1, int x2, int y2){
+double Length (int x1, int y1, int x2, int y2)
+{
 
 return Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y2-y1,2)),3);
 }
