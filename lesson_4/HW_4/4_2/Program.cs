697695799e1a76c90028df53a8d35958ff14c339
Ivan <ubahkul@gmail.com>
2023-03-23 23:57:40 +0300

    HomeWorke_4

---
+++ lesson_4/HW_4/4_2/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+// 452 -> 11        sum_all = 0 + % 10
+
+// 82 -> 10             num / 10 % 10 do 
+
+// 9012 -> 12           num > 0
+
+int SumNums(int num)
+{
+    int sum_all = 0;
+    for (; num !=0; num /= 10)
+    sum_all += num % 10;
+    return sum_all;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(SumNums(num));
