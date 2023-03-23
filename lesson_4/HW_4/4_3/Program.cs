85f1f95295aae3cb06d0d740a5632a8f51fe0c81
Ivan <ubahkul@gmail.com>
2023-03-24 00:10:09 +0300

    HomeWorke 4

---
+++ lesson_4/HW_4/4_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+// 6, 1, 33 -> [6, 1, 33]
+
+
+void Print(int[] array)
+{
+    int size = array.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+
+int[]  Mass(int size, int from, int to)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(from, to + 1);
+    return array;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = Mass(num, start, stop);
+Print(mass);
+
+
