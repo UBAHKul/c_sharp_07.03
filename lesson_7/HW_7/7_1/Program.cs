922461f61ccf7084dedb1eb1de46a32b9b6d55d9
UBAHKul <ubahkul@gmail.com>
2023-04-10 22:16:05 +0300

    8_HW

---
+++ lesson_7/HW_7/7_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// 1. Задайте двумерный массив размером m×n,
+// заполненный случайными вещественными числами.
+
+void Print(double[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j], 6} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+double[,] MassNums(int row, int column, int from, int to)
+{
+    double[,] arr = new double[row, column];
+    Random n_new = new Random();
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = Math.Round(n_new.NextDouble() * (to - from) + from, 2);            
+    return arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+double[,] mass = MassNums(row_num, column_num, start, stop);
+
+Print(mass);
---
+++ lesson_8/HW_8/8_2/Program.cs
@@ -0,0 +1,138 @@
+﻿// 2. Задайте прямоугольный двумерный массив. Напишите программу,
+//    которая будет находить строку с наименьшей суммой элементов.
+
+// ----------------------------- 1 вариант
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],4} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to + 1);
+
+    return arr;
+}
+
+int[] SumNum(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+    int[] all_nums = new int[row];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            all_nums[i] += arr[i, j];
+        }
+    }
+    return all_nums;
+}
+
+void FindMin(int[] arr)
+{
+    int min_ind = 0;
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < arr[min_ind]) min_ind = i;
+    }
+    Console.WriteLine($"Sum of elements: {arr[min_ind]}, line: {min_ind + 1}");
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+Print(mass);
+
+int[] mass_sum = SumNum(mass);
+FindMin(mass_sum);
+
+
+// ----------------------------- 2 вариант
+
+
+/*
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],3} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+void RowSmallAmount(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    double amount = double.PositiveInfinity;
+    int num;
+    int smallest = 0;
+
+    for (int i = 0; i < row_size; i++)
+    {   
+        num = 0;
+        for (int j = 0; j < column_size; j++)
+            num += arr[i, j];
+            Console.Write($"{num, 4} ");            
+
+        if (amount > num)
+        {
+            amount = num;
+            smallest = i;
+        }
+    }
+    Console.WriteLine();
+    Console.Write($"The row with the lowest amount per position - {smallest + 1}");
+}
+
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 0, 10);
+Print(arr_1);
+
+RowSmallAmount(arr_1);
+*/
